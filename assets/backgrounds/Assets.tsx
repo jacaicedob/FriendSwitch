cb4f4605cee7cf7453fde9470381ed926f1bf4cb
Jaime Caicedo <jaime.caicedo@gmail.com>
2023-05-17 23:27:42 -0400

    Added Intro scene. Created maps for the detention room and hallways

---
+++ assets/backgrounds/Assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="Assets" tilewidth="8" tileheight="8" tilecount="16384" columns="128">
+ <image source="background_spritesheet.png" width="1024" height="1024"/>
+</tileset>
